823dea2ed8f1eb97a74d319d024cc5b6434dbbd9
pengmingxing <pengmingxing.xu@qq.com>
2018-08-14 22:42:31 +0800

    完善首页。

--- api/ajax.js
+++ api/ajax.js
@@ -9,81 +9,83 @@ const api = require('./index.js')
  * success: 成功回调函数
  * error: 失败回调函数
  */
-const ajax = function (url, method, params, success, error) {
-	let tempUrl = url
-	if (typeof params === 'function') {
-		success = params;
-		error = success;
-		params = {};
-	}
-    url = `${config.domain}${url}`;
-    console.log('====请求接口====')
-    console.log(url)
-	let headers = {'Content-Type': 'application/x-www-form-urlencoded'};
-	if (!params.isLogin) {
-		headers['Cookie'] = wx.getStorageSync('session_id');
-	}
-    wx.request({
-      url: url,
-      method: method || 'post',
-      data: params,
-      header: headers,
-      success: function (res) {
-      	console.log('====成功====')
-      	console.log(res)
-        if (res.statusCode === 200) {
-          if (res.header && res.header.message === 'login') {
-            // 登录
-            console.log('====自动登录====')
-			wx.login({
-				success: res => {
-					console.log('====小程序登录接口调用成功的回调函数====')
-					console.log(res)
-					ajax(`miniProgram/user/login.do`, 'get', {
-				      code: res.code,
-				      isLogin: true
-				   }, (lres) => {
-				      if (lres.newUser) {
-				      	// 新用户跳转到授权页面
-				      	wx.redirectTo({
-			          		url: '/pages/login/index'
-			          	});
-				      	return
-				      }
-					  // 发送 res.code 到后台换取 openId, sessionKey, unionId
-					  console.log(`====登录成功, 回调ajax====`)
-					  ajax(tempUrl, method, params, success, error)
-					}, (err) => {
-					  console.log('====登录失败====')
-					  console.log(err)
-					})
-				},
-				fail: res => {
-					console.log('====小程序登录接口调用失败的回调函数====')
-					console.log(res)
-				}
-			})
-          	return;
-          }
-          if (params && params.isLogin) {
-            wx.setStorageSync('session_id', res.header['Set-Cookie'])
-          }
-          success && success(res.data)
+const ajax = function(url, method, params, success, error) {
+  let tempUrl = url
+  if (typeof params === 'function') {
+    success = params;
+    error = success;
+    params = {};
+  }
+  url = `${config.domain}${url}`;
+  console.log('====请求接口====')
+  console.log(url)
+  let headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
+  };
+  if (!params.isLogin) {
+    headers['Cookie'] = wx.getStorageSync('session_id');
+  }
+  wx.request({
+    url: url,
+    method: method || 'post',
+    data: params,
+    header: headers,
+    success: function(res) {
+      console.log('====成功====')
+      console.log(res)
+      if (res.statusCode === 200) {
+        if (res.header && res.header.message === 'login') {
+          // 登录
+          console.log('====自动登录====')
+          wx.login({
+            success: res => {
+              console.log('====小程序登录接口调用成功的回调函数====')
+              console.log(res)
+              ajax(`miniProgram/user/login.do`, 'get', {
+                code: res.code,
+                isLogin: true
+              }, (lres) => {
+                if (lres.newUser) {
+                  // 新用户跳转到授权页面
+                  wx.redirectTo({
+                    url: '/pages/login/index'
+                  });
+                  return
+                }
+                // 发送 res.code 到后台换取 openId, sessionKey, unionId
+                console.log(`====登录成功, 回调ajax====`)
+                ajax(tempUrl, method, params, success, error)
+              }, (err) => {
+                console.log('====登录失败====')
+                console.log(err)
+              })
+            },
+            fail: res => {
+              console.log('====小程序登录接口调用失败的回调函数====')
+              console.log(res)
+            }
+          })
+          return;
         }
-      },
-      fail: function (res) {
-      	console.log('====失败====')
-      	console.warn(res)
-        error && error(res)
+        if (params && params.isLogin) {
+          wx.setStorageSync('session_id', res.header['Set-Cookie'])
+        }
+        success && success(res.data)
       }
-    });
+    },
+    fail: function(res) {
+      console.log('====失败====')
+      console.warn(res)
+      error && error(res)
+    }
+  });
 };
 
-const _get = function (url, data, success, error) {
+const _get = function(url, data, success, error) {
   ajax(url, 'get', data, success, error);
 };
 
-const _post = function (url, data, success, error) {
+const _post = function(url, data, success, error) {
   ajax(url, 'post', data, success, error);
 };
 
--- pages/collection/index.js
+++ pages/collection/index.js
@@ -12,100 +12,100 @@ Page({
     businessList: []
   },
 
-  clickTab (e) {
+  clickTab(e) {
     this.setData({
       current: e.target.dataset.current
     })
   },
-  
-  getList () {
-  	// 获取我收藏的广告列表
-  	api.queryUserCollection({
-  		c_type: 1
-  	}, (res) => {
-  		this.setData({
-  			adsList: res.topics
-  		});
-  	});
-  	// 获取我收藏的商家列表
-  	api.queryUserCollection({
-  		c_type: 2
-  	}, (res) => {
-  		this.setData({
-  			businessList: res.topics
-  		});
-  	})
+
+  getList() {
+    // 获取我收藏的广告列表
+    api.queryUserCollection({
+      c_type: 1
+    }, (res) => {
+      this.setData({
+        adsList: res.topics
+      });
+    });
+    // 获取我收藏的商家列表
+    api.queryUserCollection({
+      c_type: 2
+    }, (res) => {
+      this.setData({
+        businessList: res.topics
+      });
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-		
+  onLoad: function(options) {
+
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
     this.getList();
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-   wx.stopPullDownRefresh()
+  onPullDownRefresh: function() {
+    wx.stopPullDownRefresh()
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-  
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-  
+  onShareAppMessage: function() {
+
   },
   //滑动事件处理
-  touchmove: function (e) {
-  	debugger
-		util.touchMoveEvent.call(this, e, (index, bool) => {
-			if (bool) {
-				this.data.adsList.forEach(function (v, i) {
-					v.showDel = false;
-				});
-			}
-			this.data.adsList[index].showDel = bool;
-			//更新数据
-			this.setData({
-				adsList: this.data.adsList
-			})
-		});
+  touchmove: function(e) {
+    debugger
+    util.touchMoveEvent.call(this, e, (index, bool) => {
+      if (bool) {
+        this.data.adsList.forEach(function(v, i) {
+          v.showDel = false;
+        });
+      }
+      this.data.adsList[index].showDel = bool;
+      //更新数据
+      this.setData({
+        adsList: this.data.adsList
+      })
+    });
   }
 })
